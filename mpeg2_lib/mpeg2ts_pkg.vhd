0b736531552d7d0a44be8db6dbb702bfb2bacbb2
Andreas Mueller <andym2207@gmx.de>
2015-06-05 17:58:48 +0200

    Add mpeg2_lib
    The library mpeg2_lib contains packages which offer support when
    working with MPEG2 video/audio data.

---
+++ mpeg2_lib/mpeg2ts_pkg.vhd
@@ -0,0 +1,36 @@
+--============================================================================
+--!
+--! \file      mpeg2ts_pkg
+--!
+--! \project   mpeg2_lib
+--!
+--! \langv     VHDL-2002
+--!
+--! \brief     -
+--!
+--! \details   -
+--!
+--! \bug       -
+--!
+--! \see       -
+--!
+--! \copyright GPLv2
+--!
+--! Revision history:
+--!
+--! \version   0.1
+--! \date      2015-06-06
+--! \author    Andreas Mueller
+--!
+--============================================================================
+
+
+library ieee;
+    use ieee.numeric_std.all;
+    use ieee.std_logic_1164.all;
+
+package mpeg2ts_pkg is
+end package mpeg2ts_pkg;
+
+package body mpeg2ts_pkg is
+end package body mpeg2ts_pkg;
